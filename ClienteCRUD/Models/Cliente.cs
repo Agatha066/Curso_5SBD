1959c334fec867a19ceb8a09c812147b44981195
Ágatha C F de Souza <53305979+Agatha066@users.noreply.github.com>
2023-06-05 21:06:37 -0300

    Add files via upload

---
+++ ClienteCRUD/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teste.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Este campo é preenchimento obrigatório")]
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
